1736a5feb931a84b64c26404eae66640d0a4ed8d
Onyu20230968 <ghsldeee@gmail.com>
2026-04-09 20:59:22 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("hi")
+import streamlit as st
+
+st.set_page_config(page_title="EDR Dashboard", layout="wide")
+
+st.title("EDR Dashboard")
+st.subheader("행위 로그 기반 위협 탐지 시스템")
+
+st.write("대시보드 시작 🚀")
